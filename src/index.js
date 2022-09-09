f3f1ae0e2e8d6258842596da3d96c6a4ffa05feb
SD-Jonatha <thiago.infortec@gmail.com>
2022-09-09 02:55:56 -0300

    Finalizando FinAPI

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const { request, response } = require('express');
 const express = require('express');
 const { v4: uuidv4 } = require("uuid")
 const app = express();
@@ -21,6 +22,18 @@ function verifyIfExistsAccountCPF(request, response, next){
   request.teste = 1
 
   return next();
+}
+
+function getBalance(statement){
+  const balance = statement.reduce((acc, operation)=> {
+    if(operation.type === 'credit'){
+      return acc + operation.amount;
+    }else {
+      return acc - operation.amount;
+    }
+  }, 0)
+
+  return balance
 }
 
 app.post("/account", (request, response)=> {
@@ -65,11 +78,70 @@ app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
     type: "credit"
   }
 
-  customerstatement.push(statementOperation);
+  customer.statement.push(statementOperation);
 
-  return response
+  return response.status(201).send()
 })
+ app.post("/withdraw", verifyIfExistsAccountCPF, (request, response)=>{
+  const { amount} = request.body;
+  const { customer } = request;
 
+  const balance = getBalance(customer.statement)
+
+  if(balance < amount) {
+    return response.status(400).json({error: "Insufficient funds!"})
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: "debit",
+  }
+
+  customer.statement.push(statementOperation)
+  return response.status(201).send();
+ })
+
+ app.get("/statement/date", verifyIfExistsAccountCPF, (request, response)=>{
+  const {customer} = request;
+  const {date} =request.query;
+
+  const dateFormat = new Date(date + " 00:00")
+
+  const statement = customer.statement.filter((statement)=> 
+  statement.created_at.toDateString() ===  new Date(dateFormat).toDateString())
+
+  return response.json(statement)
+
+ })
+
+ app.put("/account", verifyIfExistsAccountCPF, (request, response)=> {
+  const {name} = request.body;
+  const {customer} = request;
+  customer.name = name;
+
+  return response.status(201).send()
+ })
+
+ app.get("/account", verifyIfExistsAccountCPF, (request, response)=>{
+  const {customer} = request;
+
+  return response.json(customer)
+ })
+
+ app.get("/accountall",  (request, response)=>{
+  
+
+  return response.json(customers)
+ })
+
+ app.delete("/account", verifyIfExistsAccountCPF, (request, response)=> {
+  const {customer} = request;
+
+  customers.splice(customer, 1);
+
+  return response.status(200).json(customers)
+ })
 
 
 app.listen(3333)
